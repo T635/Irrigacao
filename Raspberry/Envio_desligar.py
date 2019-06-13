03bbfbe823f42e656c118215b58cadc9dbfa80d9
T635 <50972641+T635@users.noreply.github.com>
2019-06-13 11:58:22 -0300

    Add files via upload

---
+++ Raspberry/Envio_desligar.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+import time
+import pigpio
+import vw
+BPS=2000 
+pi = pigpio.pi()
+tx = vw.tx(pi, 25, BPS)
+compteur = 0
+start = time.time()
+
+while (time.time()-start) < 2:
+
+   compteur += 2
+
+   while not tx.ready():
+      time.sleep(0.1)
+
+   time.sleep(0.2)
+
+   while not tx.ready():
+      time.sleep(0.1)
+
+   time.sleep(0.2)
+   tx.put("1")
+   exit()
+tx.cancel()
+pi.stop()
+
